3253259dc88f8b98c6d7661b7411f0494adc8acc
Amirreza Esm <amirreza.77.esmaeili@gmail.com>
2021-01-22 23:47:11 +0330

    update server structure

--- relay-server/index.js
+++ relay-server/index.js
@@ -1,27 +1,45 @@
-// const Stream = require('node-rtsp-stream-es6')
+const Stream = require('node-rtsp-stream')
+const readline = require('readline');
+const prompt = require('prompt');
 
-// const options = {
-//   name: 'streamName',
-//   url: 'rtsp://127.0.0.1:5554/stream1',
-//   port: 5000,
-//   // ffmpeg
-// }
 
-// stream = new Stream(options)
 
-// stream.start()
-Stream = require('node-rtsp-stream')
-stream = new Stream({
-  name: 'name_f',
-  streamUrl: 'rtsp://127.0.0.1:8554/',
-  // streamUrl: 'rtsp://localhost:8989/',
-  // streamUrl: 'video.mp4',
-  wsPort: 9999,
-  ffmpegOptions: { // options ffmpeg flags
-    // '-i': 'video.mp4',
-    '-stats': '', // an option with no neccessary value uses a blank string
-    '-r': 30, // options with required values specify the value after the key
-    '-s': '720x480',
-    // '': '',
+
+const schema = {
+  properties: {
+    rtspStreamUrl: {
+      message: 'enter rtsp server url: ',
+      default: 'rtsp://127.0.0.1:8554/',
+      // default: 'rtsp://localhost:8554/',
+    },
+    websocketPort: {
+      message: 'enter websocket output port: ',
+      default: 9999,
+      type: 'integer',
+    },
   }
+}
+
+prompt.get(schema, (err, result) => {
+  if (err) {
+    console.error(err);
+  }
+  console.info(result);
+
+  const {rtspStreamUrl, websocketPort} = result;
+
+  stream = new Stream({
+    name: 'name_f',
+    streamUrl: rtspStreamUrl,
+    wsPort: websocketPort,
+    ffmpegOptions: {
+      '-stats': '',
+      '-r': 30,
+      '-s': '720x480',
+      // '': '',
+    }
+  })
 })
+
+
+
